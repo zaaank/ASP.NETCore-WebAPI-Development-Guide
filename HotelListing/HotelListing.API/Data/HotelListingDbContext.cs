0ff4ec6137c4caf6746327fca332060ddb4771fc
zaaank <zan.smo@gmail.com>
2022-06-03 14:08:10 +0200

    test
    test

---
+++ HotelListing/HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+	public class HotelListingDbContext : DbContext
+	{
+        public HotelListingDbContext(DbContextOptions options) :base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> countries { get; set; }
+	}
+}
+
